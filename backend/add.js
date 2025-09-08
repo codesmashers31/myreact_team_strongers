0f593713b7859d688f1e54f0b6373379c1007640
Bala <balasudahn17@gmail.com>
2025-09-08 19:28:53 +0530

    Node Basics

---
+++ backend/add.js
@@ -0,0 +1,10 @@
+import new_app,{new_app1} from "./new_app.js";
+
+
+const add = () => {
+    console.log(new_app());  // should print 10
+    console.log(new_app1());
+    
+};
+
+add();
---
+++ backend/new_app.js
@@ -0,0 +1,21 @@
+const new_app = ()=>{
+
+    let a = 10 
+
+return a;
+
+
+}
+
+
+export const new_app1 = ()=>{
+
+    let a = 10 +2
+
+return a;
+
+
+}
+
+
+export default new_app;
