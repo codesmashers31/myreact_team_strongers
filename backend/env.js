5cde4c4db89e23d57f7707c084afa9eb0a80ac73
Bala <balasudahn17@gmail.com>
2025-09-15 19:08:02 +0530

    File and os

---
+++ backend/env.js
@@ -0,0 +1,10 @@
+
+// import the dot env file
+import dotenv from "dotenv"; 
+
+// config the Dot env
+dotenv.config();
+
+
+// Take the variable from the env file
+console.log(process.env.APP_ENV);
