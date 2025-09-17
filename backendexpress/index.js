ee5578d4cc60c1b861b7a424c5ba8782eacc49b5
Bala <balasudahn17@gmail.com>
2025-09-17 19:06:55 +0530

    API

--- backendexpress/index.js
+++ backendexpress/index.js
@@ -1,13 +1,27 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
+import userRouters from './route/userRoutes.js'
 
+// DOTENV for env access
 dotenv.config();
 
+// Make the Express App
 const app = express();
+
+// Json Parse using text to object using USE Middleware function
+app.use(express.json());
+// Allow to all the muiltipule ports like localhost:5173 ect...
+app.use(cors());
+
 
 const PORT = process.env.PORT || 5000;
 
+// Making Route
+app.use("/api/users",userRouters)
+
+// Start the server
 app.listen(PORT,()=>{
     console.log(`Server is connected Succfully ${PORT}`);
 })
---
+++ backendexpress/route/userRoutes.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import userController from '../controller/userController.js';
+
+
+
+const router = express.Router();
+
+
+router.post("/datas",userController)
+
+
+export default router;
